a8b76a28a6760bbf69c0edebfef6a82023e228a3
YassineSalihi <y.salihi7457@uca.ac.ma>
2026-02-21 22:47:05 +0000

    add fragment temp

---
+++ app/src/main/java/com/example/convertertabsjava/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.convertertabsjava;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager;
+    ViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+
+        adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> tab.setText(position == 0 ? "Température" : "Distance")
+        ).attach();
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Quitter")
+                .setMessage("Voulez-vous vraiment quitter l'application ?")
+                .setPositiveButton("Oui", (dialog, which) -> finish())
+                .setNegativeButton("Non", null)
+                .show();
+    }
+}
---
+++ app/src/main/java/com/example/convertertabsjava/TempFragment.java
@@ -0,0 +1,4 @@
+package com.example.convertertabsjava;
+
+public class TempFragment {
+}
